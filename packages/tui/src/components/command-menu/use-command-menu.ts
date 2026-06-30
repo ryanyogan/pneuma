9d3c42de6d01d7c5f7ed6c443db560e6bdbd447a
Ryan Yogan <ryanyogan@gmail.com>
2026-06-30 18:28:45 -0500

    minor refactor

--- packages/tui/src/components/command-menu/use-command-menu.ts
+++ packages/tui/src/components/command-menu/use-command-menu.ts
@@ -33,6 +33,11 @@ export function useCommandMenu(): UseCommandMenuReturn {
     [commandQuery],
   );
 
+  const close = () => {
+    setShowCommandMenu(false);
+    pop("command");
+  };
+
   const handleContentChange = useCallback((text: string) => {
     setInputText(text);
     setSelectedIndex(0);
@@ -42,13 +47,11 @@ export function useCommandMenu(): UseCommandMenuReturn {
     if (prefix !== null && !prefix.includes(" ")) {
       setShowCommandMenu(true);
       push("command", () => {
-        setShowCommandMenu(false);
-        pop("command");
+        close();
         return true;
       });
     } else {
-      setShowCommandMenu(false);
-      pop("command");
+      close();
     }
   }, []);
 
@@ -56,8 +59,7 @@ export function useCommandMenu(): UseCommandMenuReturn {
     (index: number): Command | undefined => {
       const command = filteredCommands[index];
       if (command) {
-        setShowCommandMenu(false);
-        pop("command");
+        close();
       }
 
       return command;
@@ -71,8 +73,7 @@ export function useCommandMenu(): UseCommandMenuReturn {
     switch (key.name) {
       case "escape": {
         key.preventDefault();
-        setShowCommandMenu(false);
-        pop("command");
+        close();
 
         break;
       }
